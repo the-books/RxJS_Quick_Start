b2d146be0d4d431815f0cd4e0f140968aead9ff9
GimYoungPhil <youngphil.gim@gmail.com>
2020-04-07 16:19:26 +0900

    add ch09

---
+++ src/ch09/9-1.js
@@ -0,0 +1,44 @@
+const { fromEvent } = rxjs;
+const { ajax } = rxjs.ajax;
+const { map, mergeMap, debounceTime, filter, distinctUntilChanged, tap } = rxjs.operators;
+
+const groups$ = fromEvent(document.getElementById('inputGoup'), 'keyup')
+  .pipe(
+    debounceTime(500),
+    map(event => event.target.value.trim()),
+    distinctUntilChanged(),
+    filter(query => query.trim().length > 0),
+    tap(showLoading),
+    mergeMap(query => ajax.getJSON(`http://localhost:3000/groups?_embed=idols&groupName_like=${query}`)),
+    map(res => res[0]),
+    tap(hideLoading),
+  );
+
+groups$.subscribe(group => {
+  drawLayer(group.idols)
+});
+
+const $layer = document.getElementById('suggestLayer');
+
+function drawLayer(idols) {
+  $layer.innerHTML = idols.map(idol => {
+    return `
+    <div class="col-3 mb-3">
+      <div class="card">
+        <img src="${idol.image}" class="card-img-top" alt="...">
+        <h5 class="card-title">${idol.name}</h5>
+        <p class="card-text">${idol.height}cm, ${idol.weight}Kg</p>
+      </div>
+    </div>`;
+  }).join('');
+}
+
+const $loading = document.getElementById('loading');
+
+function showLoading() {
+  $loading.classList.add('d-block');
+}
+
+function hideLoading() {
+  $loading.classList.remove('d-block');
+}
